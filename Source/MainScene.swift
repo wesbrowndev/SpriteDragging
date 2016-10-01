14b251c5dafbfe1655b971153b3e5a6dda11a422
Wes Brown <wesbrowndev@gmail.com>
2016-10-01 09:34:52 -0400

    Added macOS support
    Added methods for mouse events used for macOS.

--- Source/MainScene.swift
+++ Source/MainScene.swift
@@ -9,6 +9,10 @@ class MainScene: CCScene {
         isUserInteractionEnabled = true
     }
     
+    // MARK: - Touch devices
+    
+    #if os(iOS)
+    
     override func touchBegan(_ touch: UITouch!, with event: UIEvent!) {
         let location = touch.location(in: self)
         _player.position = location
@@ -18,5 +22,20 @@ class MainScene: CCScene {
         let location = touch.location(in: self)
         _player.position = location
     }
-
+    
+    // MARK: - macOS
+    
+    #elseif os(OSX)
+    
+    override func mouseDown(_ theEvent: NSEvent!) {
+        let location = theEvent.location(in: self)
+        _player.position = location
+    }
+    
+    override func mouseDragged(_ theEvent: NSEvent!) {
+        let location = theEvent.location(in: self)
+        _player.position = location
+    }
+    
+    #endif
 }
